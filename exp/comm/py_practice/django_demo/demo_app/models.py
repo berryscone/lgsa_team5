fdc2151f0789b851813d1e7387adbdac48ccbd7c
rulebook <rulebook@naver.com>
2022-07-11 16:33:50 +0900

    PostgreSQL에 2500M Vehicle Database 생성하는 코드
    GIN 인덱스 생성
    CREATE INDEX demo_app_vehicledetailindexed_plate_number_trigram_index ON demo_app_vehicledetailindexed USING GIN (plate_number gin_trgm_ops);
    
    쿼리)
    SELECT * FROM demo_app_vehicledetailindexed WHERE plate_number % 'KGJ8487' ORDER BY SIMILARITY(plate_number, 'KGJ8487') DESC LIMIT 5;

--- exp/comm/py_practice/django_demo/demo_app/models.py
+++ exp/comm/py_practice/django_demo/demo_app/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 class VehicleDetail(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    plate_number = models.CharField(max_length=10)
-    owner = models.CharField(max_length=50)
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    plate_number = models.CharField(db_index=True, max_length=10, default='')
+    status = models.CharField(max_length=50, default='')
+    reg_exp = models.CharField(max_length=50, default='')
+    owner = models.CharField(max_length=50, default='')
+    birth = models.CharField(max_length=50, default='')
+    address = models.CharField(max_length=100, default='')
+    year = models.CharField(max_length=50, default='')
+    make = models.CharField(max_length=50, default='')
+    model = models.CharField(max_length=50, default='')
+    color = models.CharField(max_length=50, default='')
+
--- exp/comm/py_practice/django_demo/demo_app/views.py
+++ exp/comm/py_practice/django_demo/demo_app/views.py
@@ -1,8 +1,11 @@
+import time
+
 from django.http import JsonResponse
 
 from rest_framework import views, permissions, status
 from rest_framework.authentication import BasicAuthentication
 
+from .gen import set_vehicle_detail, set_vehicle_detail_in_video, plate_numbers_in_video
 from .models import VehicleDetail
 from .serializers import VehicleDetailSerializer
 
@@ -14,19 +17,45 @@ class LoginView(KnoxLoginView):
 
 
 class VehicleDetailView(views.APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     GET_KEY = 'license-plate-number'
 
     def get(self, request):
         params = request.GET
-        if self.GET_KEY not in params:
-            return JsonResponse({'detail': f"'{self.GET_KEY}' should be given as param"},
-                                status=status.HTTP_400_BAD_REQUEST)
+        # if self.GET_KEY not in params:
+        #     return JsonResponse({'detail': f"'{self.GET_KEY}' should be given as param"},
+        #                         status=status.HTTP_400_BAD_REQUEST)
 
         # plate_number = params[self.GET_KEY]
         # TODO: search for plate_number in database
         user = request.user
         auth = request.auth
-        vehicle_details = VehicleDetail.objects.all()
-        serializer = VehicleDetailSerializer(vehicle_details, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        # vehicle_details = VehicleDetail.objects.all()
+        # serializer = VehicleDetailSerializer(vehicle_details, many=True)
+
+        # [Start] Create DB code
+        VehicleDetail.objects.all().delete()
+
+        print("start")
+        start = time.time()
+
+        for i in range(len(plate_numbers_in_video)):
+            vehicle = VehicleDetail()
+            set_vehicle_detail_in_video(i, vehicle)
+            vehicle.save()
+
+        bulk_size = 10000
+
+        for i in range(2500):
+            bulk_list = []
+            for j in range(bulk_size):
+                vehicle = VehicleDetail()
+                set_vehicle_detail(vehicle)
+                bulk_list.append(vehicle)
+            VehicleDetail.objects.bulk_create(bulk_list)
+            print((i + 1) * bulk_size)
+
+        print("end time :", time.time() - start)
+        # [End] Create DB code
+
+        # return JsonResponse(serializer.data, safe=False)
